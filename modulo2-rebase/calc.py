1495dfbf0eac3e0f76f89e41f586a23f88533a27
ricksanchezc-137coder <jm7587290@gmail.com>
2026-07-27 19:13:40 -0300

    add funcao divide

--- modulo2-rebase/calc.py
+++ modulo2-rebase/calc.py
@@ -2,10 +2,13 @@
 def soma(a, b):
     return a + b
 
-
 def subtrai(a, b):
     return a - b
 
 def multiplica(a, b):
     """Multiplica dois numeros"""
     return a * b
+
+def divide(a, b):
+    return a / b
+
