d2af89b77897531d98dc9194de99679b378b4970
ricksanchezc-137coder <jm7587290@gmail.com>
2026-07-31 12:02:22 -0300

    modulo 6: aplica stash (soma, multiplica, divisao) via stash branch

--- modulo6-stash/app.py
+++ modulo6-stash/app.py
@@ -1,8 +1,15 @@
 def soma(a, b):
+    print(f"somando {a} e {b}")
     return a + b
 
 def multiplica(a, b):
+    print(f"multiplicando {a} e {b}")
     return a * b
 
 def media(lista):
     return sum(lista) / len(lista)
+
+def divisao(a, b):
+    print(f"dividindo {a} e {b}")
+    return a / b
+
